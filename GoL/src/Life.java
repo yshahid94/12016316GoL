228af4c2fb7f8ac79f7e45476d535ad6e0ae1184
Yassin Shahid <yshahid94@gmail.com>
2015-03-18 20:18:06 +0000

    v0.3
    Displaying grid on JPanel, with Play/Pause and Step controls

--- GoL/src/Life.java
+++ GoL/src/Life.java
@@ -1,29 +1,68 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class Life {
+import javax.swing.JPanel;
 
-	 boolean[][] Cells = new boolean[][] {
-		{false,true,false,false,false},
-		{false,false,true,false,false},
-		{true,true,true,false,false},
-		{false,false,false,false,false},
-		{false,false,false,false,false}
-	};	
-	 int ylength = Cells.length;
-	 int xlength = Cells[0].length;
+
+public class Life extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	//Variables
+	private Color cellOff = Color.BLACK;
+	private Color cellOn = Color.WHITE;
+	boolean[][] Cells = new boolean[][] {
+			{false,true,false,false,false},
+			{false,false,true,false,false},
+			{true,true,true,false,false},
+			{false,false,false,false,false},
+			{false,false,false,false,false}
+		};	
+	int ylength = Cells.length;
+	int xlength = Cells[0].length;
+	int cellSize = 20;
+	int boardSize = 5;
+	int speed = 50;												//The speed at which the automatic generations happen, 1 = 1fps, 2 = 2fps...etc
+	private Timer timer;
+		
+	Life(){
+	}
 	
-	public Life()
+	/**
+	 * Handles all of the drawing onto the Panel
+	 */
+	public void paintComponent(Graphics g)
 	{
+		super.paintComponent(g);
+		
+		//Draw Board
+		for (int i = 0 ; i < boardSize; i++){			//Going through the Y axis
+			for (int j = 0 ; j < boardSize; j++){		//Going through the X axis
+				if (Cells[i][j]){
+					g.setColor(cellOn);
+				}
+				else{
+					g.setColor(cellOff);
+				}
+				g.fillRect(offsetcalc(j), offsetcalc(i), cellSize, cellSize);
+			}
+		}
 	}
 	
-	public boolean[][] getCells(){
-		return Cells;
+	public int offsetcalc(int point){
+		return point+(cellSize*(point)+1);
 	}
-
+	
+	//Background Stuff
 	public int CheckNeighbours (int x, int y){
 		int[][] neighboursArray= {
-				{-1,-1},{0,-1},{1,-1},
-				{-1,0},{1,0},
-				{-	1,1},{0,1},{1,1}				
+				{-1,-1},	{0,-1},		{1,-1},
+				{-1,0},						{1,0},
+				{-	1,1},		{0,1},		{1,1}				
 		};
 		int neighbours = 0;
 		for (int[] i : neighboursArray){
@@ -38,7 +77,6 @@ public class Life {
 	}
 		
 	public void generate(){
-		
 		boolean[][] tempCells = new boolean[ylength][xlength];
 		for (int i = 0; i < ylength; i++){
 			for (int j = 0; j < xlength; j++){
@@ -57,15 +95,18 @@ public class Life {
 		Cells = tempCells;
 	}
 	
-//	public void display(){
-//		for (int i = 0; i < ylength; i++){
-//			for (int j = 0; j < xlength; j++){
-//			if (Cells[i][j] == true){ System.out.print("X\t");}
-//			else System.out.print("-\t");
-//			}
-//			System.out.println();
-//		}
-//	}
+	public void startGenerating() {
+	    timer = new Timer();
+	    timer.schedule( new TimerTask(){
+	        public void run(){
+	            generate();
+	            repaint();
+	        }
+	    },0,  1000/speed ); 
+	}
 	
-
+	public void stopGenerating() {
+	   timer.cancel();
+	}
+	
 }
--- GoL/src/aFrame.java
+++ GoL/src/aFrame.java
@@ -1,10 +1,92 @@
+ import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JTextArea;
+import javax.swing.border.Border;
 
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class aFrame extends JFrame {
 
-	static Life life = new Life();
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	
+
+	private JMenuBar menubar;
+	private JMenu fileMenu;
+	private JMenuItem playItem, stepItem, clearItem;
+	private Boolean playing = false;
+	Life life = new Life();
+
+	GridBagConstraints c = new GridBagConstraints();
 	
+	public aFrame(){
+		setLayout(new GridBagLayout());
+
+		menubar = new JMenuBar();
+		this.setJMenuBar(menubar);
+		
+		playItem = new JMenuItem("Start/Stop");
+	    playItem.addActionListener
+		(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				if (playing.equals(false)){
+					playing = true;
+					life.startGenerating();
+				}
+				else{
+					playing = false;
+					life.stopGenerating();
+				}
+			}
+		}
+		);
+	    
+	    stepItem = new JMenuItem("Step");
+	    stepItem.addActionListener
+		(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				life.generate();
+				life.repaint();
+			}
+		}
+		);
+	    
+	    clearItem = new JMenuItem("Clear");
+	    clearItem.addActionListener
+		(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				//Code Here
+			}
+		}
+		);
+		
+	    //File menu
+  		fileMenu = new JMenu("File");
+  		menubar.add(fileMenu);
+		fileMenu.add(playItem);
+		fileMenu.add(stepItem);
+		fileMenu.add(clearItem);
+			
+		//Panel Constructor
+		c.weightx=1;
+		c.weighty=1;
+		c.fill = GridBagConstraints.BOTH;
+	    getContentPane().add(life, c);
+	}
 }
